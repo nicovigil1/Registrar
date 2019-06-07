df92384267b761a59a0fea9b8e88d4d0b7cf5142
Nico <nicovigil1@gmail.com>
2019-06-06 21:53:19 -0500

    cleanup validation spacing for readability

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,8 @@
 class Participant < ApplicationRecord
+  # Validations
   validates_presence_of :name
   validates_uniqueness_of :name
+
   validates_presence_of :gender
   validates_presence_of :birthday
   validates_presence_of :enrollment
@@ -8,6 +10,7 @@ class Participant < ApplicationRecord
   validates_presence_of :email
   validates_presence_of :phone
 
+  # Associations
   has_many :participant_registries
   has_many :registries, through: :participant_registries
 end
