1ab9861f4140ce543d752d5a91a057714fcc2e07
Nico <nicovigil1@gmail.com>
2019-06-07 00:02:34 -0500

    passing test for user login

--- spec/features/user/user_can_login_spec.rb
+++ spec/features/user/user_can_login_spec.rb
@@ -1,45 +1,68 @@
 require 'rails_helper'
 
-describe "A user" do
+describe "A user" do  
   it "can visit the homepage" do
     visit root_path
     
     expect(page).to have_content("Welcome to Registrar!") 
   end
 
-  xit 'can login' do 
+  it 'can login' do 
     user = User.create(email: "email@email.com", phone: "8675309", name: "Delta Dawn")
 
     visit root_path
 
     within "#login" do
-      expect(page).to have_content("Enter Email to Login") 
+      expect(page).to have_content("Enter Email") 
       
-      fill_in "Enter Email to Login",	with: "email@email.com"
+      fill_in "Enter Email",	with: "email@email.com"
+      click_on "Login"
     end
 
     expect(current_path).to eq(user_path(user)) 
   end
 
-  xit "can register" do
+  it "can register" do
     visit root_path
     
     within "#login" do
-      expect(page).to have_content("Enter Email to Login") 
+      expect(page).to have_content("Enter Email") 
       expect(page).to have_content("Register")
 
       click_on "Register"
 
       expect(current_path).to eq(new_user_path)
-
-      fill_in "Email",	with: "email@email.com"
-      fill_in "Phone",	with: "8675309"
-      fill_in "Name (first & last)",	with: "Delta Dawn" 
     end
+    
+    fill_in "Name (first & last)",	with: "Delta Dawn"
+    fill_in "Email",	with: "email@email.com"
+    fill_in "Phone",	with: "8675309"
+    
+    click_on "Submit"
 
     new_user = User.find_by(email: "email@email.com")
 
     expect(new_user.class).to eq(User)
     expect(current_path).to eq(user_path(new_user)) 
+  end
+
+  it "can't register with bad credentials" do
+    visit root_path
+    
+    within "#login" do
+      expect(page).to have_content("Enter Email") 
+      expect(page).to have_content("Register")
+
+      click_on "Register"
+
+      expect(current_path).to eq(new_user_path)
+    end
+
+    fill_in "Name (first & last)",	with: "Delta Dawn"
+    fill_in "Email",	with: "email@email.com"
+    
+    click_on "Submit"
+    
+    expect(page).to have_content("Phone can't be blank") 
   end
 end
